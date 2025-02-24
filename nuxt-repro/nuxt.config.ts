e19226b4e43efd27daebd049b043cee50bd04a16
Kat <hi.connkat@gmail.com>
2025-02-24 12:24:06 -0700

    added current setup

--- nuxt-repro/nuxt.config.ts
+++ nuxt-repro/nuxt.config.ts
@@ -1,23 +1,112 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const isProduction = !!process.env.IS_PRODUCTION;
+
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+	ssr: true,
 
-  future: {
-    compatibilityVersion: 4,
-  },
+	modules: [
+		'@antfu/eslint-config',
+		'@nuxtjs/color-mode',
+		'@nuxtjs/device',
+		'@nuxt/image',
+		'@nuxtjs/i18n',
+		'@nuxtjs/google-fonts',
+		'@nuxtjs/sanity',
+		'@nuxtjs/seo',
+		'@nuxtjs/tailwindcss'
+	],
 
-  modules: ["@nuxtjs/sanity"],
+	seo: {
+		automaticDefaults: false,
+	},
+	site: {
+		url: 'https://hiboop.com',
+		name: 'HiBoop!',
+		favicon: true,
+	},
+	sanity: {
+		projectId: process.env.NUXT_SANITY_PROJECT_ID || 'm95ktlhh',
+		dataset: process.env.NUXT_SANITY_DATASET,
+		useCdn: false,
+		apiVersion: process.env.NUXT_SANITY_API_VERSION || '2025-02-11',
+		visualEditing: {
+			studioUrl:
+				process.env.NUXT_SANITY_STUDIO_URL || 'http://localhost:3333',
+			token: process.env.NUXT_SANITY_API_READ_TOKEN,
+		},
+	},
 
-  sanity: {
-    apiVersion: "2025-02-13",
-    projectId: "3jds3t8i",
-    dataset: "production",
-    visualEditing: {
-      studioUrl: "http://localhost:3333",
-      token:
-        "skKm1OissDdk5gILoLTaejGJa72bKFvabsnxi9vnV2FWzNTgAhj4AjlVQyMLN30Z7fBVU9DsHHemBH15q9dqd33pbwFGekYmviMPtheFzy3r8mEgZwe7zBOQxfBGSd38XVbOu6I9rdTGTpdXMhgHUkHdoyjNOSDmY0xzBDjYSe2d1JEUPO7l",
-    },
-  },
 
-  compatibilityDate: "2025-02-13",
+	app: {
+		baseURL: '/',
+		pageTransition: {
+			name: 'page',
+			mode: 'in-out',
+		},
+	},
+
+	image: {
+		dir: 'assets/img',
+		format: ['avif', 'webp'],
+	},
+
+	colorMode: {
+		classSuffix: '',
+		dataValue: 'theme',
+	},
+
+	nitro: {
+		firebase: {
+			gen: 2,
+			nodeVersion: '20',
+		},
+		routeRules: {
+			'/insights/**': { ssr: true },
+		},
+	},
+
+	css: ['~/assets/css/tailwind.css', '~/assets/css/main.css'],
+	typescript: { strict: false },
+
+	runtimeConfig: {
+		postmarkApiKey: process.env.NUXT_POSTMARK_API_KEY || '',
+		public: {
+			siteUrl: 'https://hiboop.com',
+			isProduction,
+			sanity: {
+				projectId: process.env.NUXT_SANITY_PROJECT_ID || 'm95ktlhh',
+			},
+		},
+	},
+
+	experimental: {
+		restoreState: true,
+	},
+
+	devtools: {
+		enabled: false,
+	},
+
+	vite: {
+		// workaround for open issue:
+		// https://github.com/nuxt-modules/sanity/issues/1159#issuecomment-2662477319
+		optimizeDeps: {
+			include: ['shallowequal', 'lodash/startCase.js'],
+		},
+		server: {
+			watch: {
+				usePolling: true,
+			},
+			hmr: {
+				port: 3000,
+			},
+			cors: true,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': '*',
+				'Access-Control-Allow-Headers': '*',
+			},
+		},
+	},
+
+	compatibilityDate: '2024-08-12',
 });
