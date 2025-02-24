472d3759b5036256afc86b9e2261ae4fdc4bfd22
Kat <hi.connkat@gmail.com>
2025-02-24 12:23:28 -0700

    added current setup

--- sanity-repro/sanity.cli.ts
+++ sanity-repro/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '3jds3t8i',
+    projectId: 'm95ktlhh',
     dataset: 'production'
   },
   /**
--- sanity-repro/sanity.config.ts
+++ sanity-repro/sanity.config.ts
@@ -1,31 +1,97 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
-import {presentationTool} from 'sanity/presentation'
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
+import { type DocumentLocation,	defineDocuments,	defineLocations,	presentationTool } from 'sanity/presentation';
+import { schemaTypes } from './schemaTypes';
+
+const homeLocation = {
+	title: 'Home',
+	href: '/',
+} satisfies DocumentLocation;
+
+export const projectId = 'm95ktlhh';
+export const dataset =  'production';
+
+function resolveHref(documentType?: string, id?: string): string | undefined {
+	switch (documentType) {
+		case 'post':
+			return id ? `/insights/${id}` : undefined;
+		case 'page':
+			return id ? `/${id}` : undefined;
+		default:
+			console.warn('Invalid document type:', documentType);
+			return undefined;
+	}
+}
 
 export default defineConfig({
-  name: 'default',
-  title: 'test project',
+	name: 'hiboop-site',
+	title: 'HiBoop Site',
+	projectId,
+	dataset,
+	plugins: [structureTool(), visionTool(), presentationTool({
+		previewUrl: {
+			preview: '/insights',
+			origin: 'https://hiboop.com',
+			previewMode: {
+				enable: '/preview/enable',
+				disable: '/preview/disable',
+			},
+		},
+		resolve: {
+			mainDocuments: defineDocuments([
+				{
+					route: '/insights',
+					filter: '_type == "post"',
+				},
+				{
+					route: '/insights/:slug',
+					filter: '_type == "post" && slug.current == $slug',
+				},
+			]),
+			locations: {
+				settings: defineLocations({
+					locations: [homeLocation],
+					message: 'This document is used on all pages',
+					tone: 'positive',
+				}),
+				page: defineLocations({
+					select: {
+						name: 'name',
+						slug: 'slug.current',
+					},
+					resolve: doc => ({
+						locations: [
+							{
+								title: doc?.name || 'Untitled',
+								href: resolveHref('page', doc?.slug)!,
+							},
+						],
+					}),
+				}),
+				post: defineLocations({
+					select: {
+						title: 'title',
+						slug: 'slug.current',
+					},
+					resolve: doc => ({
+						locations: [
+							{
+								title: doc?.title || 'Untitled',
+								href: resolveHref('post', doc?.slug)!,
+							},
+							{
+								title: 'Home',
+								href: '/',
+							} satisfies DocumentLocation,
+						].filter(Boolean) as DocumentLocation[],
+					}),
+				}),
+			},
+		},
+	})],
 
-  projectId: '3jds3t8i',
-  dataset: 'production',
-
-  plugins: [
-    structureTool(),
-    visionTool(),
-    presentationTool({
-      previewUrl: {
-        origin: 'http://localhost:3000',
-        previewMode: {
-          enable: '/preview/enable',
-          disable: '/preview/disable',
-        },
-      },
-    }),
-  ],
-
-  schema: {
-    types: schemaTypes,
-  },
-})
+	schema: {
+		types: schemaTypes,
+	},
+});
